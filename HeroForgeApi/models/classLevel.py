2e587ea43d52fc0d4c0fb1f4c777fe3b73f375fa
Sean Derevjanik <sean.derevjanik@gmail.com>
2022-03-17 08:41:37 -0700

    implemented race views

--- HeroForgeApi/models/classLevel.py
+++ HeroForgeApi/models/classLevel.py
@@ -3,11 +3,11 @@ from django.db import models
 
 
 class ClassLevel(models.Model):
-    classId = models.ForeignKey("Classs", on_delete=models.CASCADE)
+    classs = models.ForeignKey("Classs", on_delete=models.CASCADE)
     level = models.PositiveSmallIntegerField()
     features = models.TextField()
     fixedFeat = models.ForeignKey("Feat", on_delete=models.SET_NULL, null=True, blank=True)
-    featSetId = models.ForeignKey("FeatSet", on_delete=models.SET_NULL, null=True, blank=True)
+    featSet = models.ForeignKey("FeatSet", on_delete=models.SET_NULL, null=True, blank=True)
     BAB = models.PositiveSmallIntegerField()
     Fort = models.PositiveSmallIntegerField()
     Ref = models.PositiveSmallIntegerField()
