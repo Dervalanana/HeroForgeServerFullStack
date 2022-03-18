bde3f7a50ce9df80c073e23018105ddcb59e2b2b
Sean Derevjanik <sean.derevjanik@gmail.com>
2022-03-18 12:42:01 -0700

    cleaned trailing info

--- HeroForgeApi/views/equipment.py
+++ HeroForgeApi/views/equipment.py
@@ -4,7 +4,7 @@ from rest_framework.viewsets import ViewSet
 from rest_framework.response import Response
 from rest_framework.decorators import action
 from rest_framework import serializers, status
-from HeroForgeApi.models import Equipment, Character, Equipped, character, equipment
+from HeroForgeApi.models import Equipment, Character, Equipped
 
 
 class EquipmentView(ViewSet):
